75fb1774add98e1642bba8ed19add4d3a53c6814
Abigailvantol <abigailvantol@gmail.com>
2013-04-16 15:58:39 +0200

    classes + assets

--- my-gdx-game-android/src/com/me/mygdxgame/MainActivity.java
+++ my-gdx-game-android/src/com/me/mygdxgame/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends AndroidApplication {
         super.onCreate(savedInstanceState);
         
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = false;
+        cfg.useGL20 = true;
+        cfg.useAccelerometer = true;
         
         initialize(new MyGdxGame(), cfg);
     }
